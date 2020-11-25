6ebb21024ca605f901acc2c39cc949070b4db179
Tejas Nakhate <eanakhate@gmail.com>
2020-11-25 23:34:19 +1100

    Add files via upload

---
+++ FilesCreated.psm1
@@ -0,0 +1,16 @@
+﻿function Get-NewFiles
+{
+    param (
+    [string]$Seconds = 60,
+    [string]$Path = "C:\"
+    )
+    $now = Get-Date
+    foreach($i in Get-ChildItem -Path $Path -Recurse)
+    {
+        $elapsed = (New-TimeSpan -Start $i.CreationTime -End $now).TotalSeconds
+        if($elapsed -le $Seconds)
+        {
+            Write-Host $i.FullName
+        }
+    }
+}
